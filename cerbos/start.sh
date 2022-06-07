f95b1e06d2fe8c6574c22262434b66b9253c75ee
Alex Olivier <alex@alexolivier.me>
2022-06-07 15:07:13 +0100

    new sdk

--- cerbos/start.sh
+++ cerbos/start.sh
@@ -1,5 +1,5 @@
 docker run -i -t -p 3592:3592 \
   -v $(pwd)/config:/config \
   -v $(pwd)/policies:/policies \
-  ghcr.io/cerbos/cerbos:0.12.0 \
+  ghcr.io/cerbos/cerbos:latest \
   server --config=/config/conf.yaml
